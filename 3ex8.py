1992c2060605d4a333b1ca38a39ac7d7e9c2e961
Colin Welles <colin.l.welles@gmail.com>
2018-02-26 13:07:28 -0500

    my commit

---
+++ 3ex8.py
@@ -0,0 +1,13 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+#Taking formatter string on line 1, calls its format function, fourmat four arguments which match the four {}
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+        "Try your",
+        "Own text here",
+        "Maybe a poem",
+        "Or a song about fear"
+))
